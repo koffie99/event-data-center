1c23077d9801074c9928c2add85be88b22113ea4
Joshua <koffworld@gmail.com>
2025-05-15 20:04:13 +0000

    cordinated

--- app/(screens)/portal/page.jsx
+++ app/(screens)/portal/page.jsx
@@ -1,16 +1,42 @@
 "use client"
 import Image from "next/image"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { LuLayoutDashboard } from "react-icons/lu"
 import { HiOutlineDocumentDownload } from "react-icons/hi"
 import { MdOutlineSms } from "react-icons/md"
 import { HiOutlineMail } from "react-icons/hi"
+import Dashboard from "@/app/(modules)/Dashboard"
+import Generate from "@/app/(modules)/Generate"
+import SMS from "@/app/(modules)/SMS"
+import Email from "@/app/(modules)/Email"
 
 const Portal = () => {
+  const [currentPage, setCurrentPage] = useState("dashbaord")
+
+  const setPage = (page) => {
+    setCurrentPage(page)
+  }
+
+  // custom page
+  const customPage = () => {
+    switch (currentPage) {
+      case "dashboard":
+        return <Dashboard />
+      case "generate":
+        return <Generate />
+      case "sms":
+        return <SMS />
+      case "email":
+        return <Email />
+      default:
+        return <Dashboard />
+    }
+  }
+
   return (
     <div className="flex bg-[#f9fafd] min-h-screen">
       {/* sidenav */}
-      <div className="flex-[0.2] bg-white p-8">
+      <div className="flex-[0.2] bg-white p-8 shadow">
         <Image
           width={100}
           height={100}
@@ -18,20 +44,40 @@ const Portal = () => {
           src="/images/logo.png"
           className="mx-auto ml-[-10px]"
         />
-        <div className="mt-5 flex flex-col gap-2">
-          <div className="active flex items-center gap-3 rounded-lg">
+        <div className="mt-5 flex flex-col gap-3">
+          <div
+            className={`${
+              currentPage === "dashboard" ? "active" : "nav"
+            } flex items-center gap-3 rounded-lg cursor-pointer p-2`}
+            onClick={() => setPage("dashboard")}
+          >
             <LuLayoutDashboard />
             <p>Dashboard</p>
           </div>
-          <div className="flex items-center gap-3">
+          <div
+            className={`${
+              currentPage === "generate" ? "active" : "nav"
+            } flex items-center gap-3 rounded-lg  cursor-pointer p-2`}
+            onClick={() => setPage("generate")}
+          >
             <HiOutlineDocumentDownload />
             <p>Generate Data</p>
           </div>
-          <div className="flex items-center gap-3">
+          <div
+            className={`${
+              currentPage === "sms" ? "active" : "nav"
+            } flex items-center gap-3 rounded-lg  cursor-pointer p-2`}
+            onClick={() => setPage("sms")}
+          >
             <MdOutlineSms />
             <p>SMS Zone</p>
           </div>
-          <div className="flex items-center gap-3">
+          <div
+            className={`${
+              currentPage === "email" ? "active" : "nav"
+            } flex items-center gap-3 rounded-lg  cursor-pointer p-2`}
+            onClick={() => setPage("email")}
+          >
             <HiOutlineMail />
             <p>Email Zone</p>
           </div>
@@ -41,10 +87,10 @@ const Portal = () => {
       {/* rightside content */}
       <div className="flex-[0.8]">
         {/* top nav */}
-        <div></div>
+        <div className="h-[10vh] bg-white shadow w-full"></div>
 
         {/* content area */}
-        <div></div>
+        <div className="p-6">{customPage()}</div>
       </div>
     </div>
   )
